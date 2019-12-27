c81d450852926a1b6aba84db68e79bba00cec54c
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2019-12-27 23:18:56 +0100

    not work

--- src/dewikiparser.ts
+++ src/dewikiparser.ts
@@ -16,91 +16,107 @@ export function parseWikiDump(dumpFile:string, insertEntry: (entry:Entry) => voi
     let entry:Entry = {
         word: "word",
         text: "TEXT"
-    };
-    let inPage = false;
-    xml.preserve('text');
-    /*xml.on("text: text", (element:any)=>{
-        console.log(element);
-    });
-    */
+    };    
+    xml.preserve('text');    
     
     xml.on("endElement: page", (element:any) => {
         let ns = element["ns"];
         console.log({ns});
         if (ns === '0') {
             let title =  element["title"];
-            console.log("===========|"+title);
+            console.log("===========|"+title);            
             let originText = element["revision"]["text"]["$children"];
-            let text = joinText(originText);
-            if (title === "Januar") {
-                console.log( text );
+            try{
+                let text = joinText(originText);
+                //if (title === "ordo") {
+                    console.log( text );
+                //}
+            }catch(ex) {
+                console.log(ex);
             }
         }
     });    
     
 }
 
-const entities = [
-    "<", ">", "'", '"', '&'
-];
+
 
 function joinText(text:string[]):string {
     let cleanup:string[] = [];
-    let isLookForward = false;
-    let buffer: string[] = [];
-    let tags:string[] = [];
-    let pushTagName = false;
+    let xmlTag:string[] = [];
+    let onBuildXmlTag = false;
+    let inXMLElement = false;
+    let xmlElement:string[][] = [];
     for(let line of text) {
-        if ( isLookForward ) {
-            buffer.push(line);
-        } else {
-            cleanup.push(line);
-        }
-        if (line === "<") {            
-            if (!isLookForward) {
-                buffer.push(line);
-                isLookForward = true;
+        let preProcess = line;
+        if (line === "<") {
+            onBuildXmlTag = true;
+        } else if (line === ">") {
+            onBuildXmlTag = false;
+            let currentXMLTag = xmlTag.join("") + line;
+            //console.log("currentXMLTag: " + currentXMLTag);
+            xmlTag = [];
+            if (isXMLOpenTag(currentXMLTag)) {
+                inXMLElement = true;
+                let currentElement = [currentXMLTag];
+                xmlElement.push(currentElement);                
+                continue;
+            } else if (isXMLCloseTag(currentXMLTag)) {
+                let currentElement = xmlElement.pop();                
+                currentElement!.push(currentXMLTag); // verdammt scheiße check
+                let elementText = currentElement!.join("");
+                inXMLElement = xmlElement.length > 0;
+                //console.log(`-----------> xmlElement.length ${xmlElement.length} ` + elementText);
+                //continue;
+                preProcess = elementText;
+            } else if(isXMLSimpleTag(currentXMLTag)){
+                //console.log(`-----------> single element  ` + currentXMLTag);
+                preProcess = currentXMLTag;
+                //continue;
+            } else {
+                //console.log("Cannot recognize tag " + currentXMLTag);
+                continue;
             }
-            pushTagName = true;
-            continue;
-        }
-        if(pushTagName) {
-            let tag = line.split(/\s+/)[0];
-            console.log("new tag: " + tag);
-            tags.push(tag);
-            pushTagName = false;
         }
 
-        if (line === ">" && isLookForward) {
-            console.log("see a >");
-            let tag = tags[tags.length-1];            
-            let lastBuffer = buffer[buffer.length-2];
-            console.log({tag, lastBuffer});
-            if (lastBuffer === "/"){ // end of a single tag
-                isLookForward = false;            
-                let bufferLine = buffer.join("");
-                console.log("single tag:---> " + bufferLine);
-                cleanup.push(bufferLine);
-                buffer = [];
-                tags.pop();
-            }else if (lastBuffer === '"' || tag === lastBuffer) { // end of a tag-open within text inside
-                let bufferLine = buffer.join("");
-                console.log("end of tag-open:-----> " + bufferLine);
-                cleanup.push(bufferLine);
-                buffer = [];
-            } else {                
-                if ( "/" + tag === lastBuffer ) { // end of a tag-close within text inside
-                    isLookForward = false;
-                    let bufferLine = buffer.join("");
-                    cleanup.push(bufferLine);
-                    console.log("end of tag-close: ----> " + bufferLine);
-                    buffer = [];
-                    tags.pop();
-                } else {
-                    console.log("===WTF=== why I see " + lastBuffer);
-                }
-            }
+        if (onBuildXmlTag) {
+           //xmlTag.push(line);
+            pushOrAppend(line, xmlTag);
+        } else if (inXMLElement) { // not by building a xml tag (open or close)
+            let topXMLElement = xmlElement[xmlElement.length - 1];
+            pushOrAppend(line, topXMLElement);
+        } else {
+            //cleanup.push(preProcess);
+            pushOrAppend(preProcess, cleanup);
         }
     }
     return cleanup.join("\n");
+}
+
+function isXMLOpenTag  (xmlTag:string) : boolean {
+    return xmlTag.search("</") === -1 && xmlTag.search("/>")===-1;
+}
+
+function isXMLCloseTag(xmlTag:string):boolean {
+    return xmlTag.startsWith("</");
+}
+
+function isXMLSimpleTag(xmlTag:string):boolean {
+    return xmlTag.endsWith("/>");
+}
+
+
+function pushOrAppend(line:string,target:string[]) {
+    if(line === "&") {
+        let lastElement = target[target.length-1];
+        lastElement += line;
+        target[target.length-1] = lastElement;        
+    }else if (line.startsWith("{{") || line.startsWith("==== {{")) {
+        console.log("-------> append extra newline" + "\n" + line );
+        target.push("\n");
+        target.push(line);
+        // console.log(target);
+    } else {
+        target.push(line);
+    }
 }
